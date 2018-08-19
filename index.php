5e0128e9fec4d6089bf2e4d2494c9dc2b60b06f0
TCHProgrammer <TCHProgrammer@rambler.ru>
2018-08-19 17:05:36 +0300

    corriges

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 		<script type="text/javascript" src="slick-master/slick/slick.min.js"></script>
 	</head>
-	<body >
+	<body style="background-color:gray;">
 		<script type="text/javascript">
 			$(document).ready(function(){
 				
